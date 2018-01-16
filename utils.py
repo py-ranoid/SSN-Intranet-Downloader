53a93caaca96f70a3e01aef622768b1184cd9fc0
Vishal Gupta <vishalg8897@gmail.com>
2018-01-16 17:16:41 +0530

    Changed paths to work locally

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import git
 import datetime
 import glob
+import os
 
 r = git.Repo('.')
 remote_parts = r.remotes.origin.url[:-4].split('/')
@@ -13,11 +14,11 @@ allhtmls = glob.glob('*/*/*.html') + glob.glob('*/*.html')
 for f in allhtmls:
     fl = open(f)
     content = fl.read()
-    content = content.replace(curr, newpath)
+    content = content.replace(newpath, curr)
     open(f, 'w').write(content)
 
 r.index.add(['CseElearnThirdYear/*'])
 r.index.commit('Added files - ' + str(datetime.datetime.now()))
 
-org = r.remotes.origin
-org.push('master')
+# org = r.remotes.origin
+# org.push('master')
